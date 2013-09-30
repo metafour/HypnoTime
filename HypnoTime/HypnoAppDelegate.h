ef9ef8f7f04f4b7395f8826a3bbacd37e1a0e7f0
Camron Schwoegler <camron@metafour.org>
2013-09-30 11:44:17 -0600

    Initial Commit

---
+++ HypnoTime/HypnoAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnoAppDelegate.h
+//  HypnoTime
+//
+//  Created by Camron Schwoegler on 9/30/13.
+//  Copyright (c) 2013 Camron Schwoegler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnoAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HypnoTime/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HypnoTime
+//
+//  Created by Camron Schwoegler on 9/30/13.
+//  Copyright (c) 2013 Camron Schwoegler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HypnoAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HypnoAppDelegate class]));
+    }
+}
